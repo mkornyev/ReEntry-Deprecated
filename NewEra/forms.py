6d45907f949186e3133d71070739d42c62c96a27
Joey Perrino <epiccrashv3@gmail.com>
2020-03-21 03:20:38 -0400

    Model changes to reflect last meeting
    - Dates changed to hours of operation
    - Secondary street address added

--- NewEra/forms.py
+++ NewEra/forms.py
@@ -101,9 +101,11 @@ class CreateResourceForm(forms.ModelForm):
 	name = forms.CharField(max_length=100, required=True)
 	description = forms.CharField(max_length=1000, widget=forms.Textarea(attrs=INPUT_ATTRIBUTES))
 	is_active = forms.BooleanField(required=False)
+	hours = forms.CharField(max_length=1000, required=False, widget=forms.Textarea(attrs=INPUT_ATTRIBUTES))
 	email = forms.EmailField(max_length=254, required=False)
 	phone = forms.CharField(max_length=10, required=False)
 	street = forms.CharField(max_length=100, required=False)
+	street_secondary = forms.CharField(max_length=100, required=False)
 	city = models.CharField(max_length=100)
 	zip_code = forms.CharField(max_length=10, required=False)
 	state = forms.CharField(max_length=2, required=False)
@@ -111,7 +113,7 @@ class CreateResourceForm(forms.ModelForm):
 
 	class Meta:
 		model = Resource
-		fields = ('name', 'description', 'is_active', 'start_date', 'end_date', 'email', 'phone', 'street', 'zip_code', 'state', 'image', 'url', 'tags')
+		fields = ('name', 'description', 'is_active', 'hours', 'email', 'phone', 'street', 'street_secondary', 'zip_code', 'state', 'image', 'url', 'tags')
 		exclude = (
 			'content_type',
 		)
