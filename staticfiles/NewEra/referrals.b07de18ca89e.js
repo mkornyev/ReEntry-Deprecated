3ff712f132dd96835345e6f2eb1458869f9cbe4d
Brent Hong <brenth@andrew.cmu.edu>
2020-04-29 00:00:57 -0400

    Added more front end

---
+++ staticfiles/NewEra/referrals.b07de18ca89e.js
@@ -0,0 +1,123 @@
+var stagedDeleteId = null;
+
+$(document).ready(function() {
+    $('#make-referral').attr('state', 'off');
+    $('#make-referral').click(toggleSelect);
+    $('#referral-ins').css('display', 'none');
+});
+
+cancel = () => {
+    var delModal = document.getElementById('del-confirm');
+    delModal.style.display = "none";
+}
+
+confirmDelete = () => {
+    console.log(localStorage.stagedResources);
+    if (stagedDeleteId == null) {
+        // Fail silently: just close the modal, however this
+        // should not happen.
+        var delModal = document.getElementById('del-confirm');
+        delModal.style.display = "none";
+        return;
+    }
+
+    stagedResources = JSON.parse(localStorage.getItem("stagedResources"));
+    leftoverResources = stagedResources.filter((id) => id != stagedDeleteId);
+
+    var empty = false;
+    if (leftoverResources.length === 0) {
+        empty = true;
+    }
+
+    localStorage.stagedResources = JSON.stringify(leftoverResources)
+
+    if (empty) {
+        document.location.href = "/resources";
+    }
+
+    else {
+        document.location.href = '/create_referral?resources=' + localStorage.stagedResources;
+    }
+}
+
+deleteResource = (id) => {
+    stagedDeleteId = id;
+    var delModal = document.getElementById('del-confirm');
+    delModal.style.display = "block";
+}
+
+function toggleSelect () {
+
+    localStorage.stagedResources = "[]";
+
+    if ($(this).attr('state') === 'off') {
+        $('.resource-card').each(function() {
+            $(this).attr('state','out')
+            $(this).click(toggleItem)
+        }) 
+
+        $(this).html('Cancel')
+        $(this).attr('state', 'on')
+        $('#commit-referral').css('display', 'block');
+        let refCount = 0;
+        document.getElementById("commit-referral").innerHTML = "Select Resources " + "(" + refCount + ")"
+        $('#referral-ins').css('display', 'block')
+    } else if ($(this).attr('state') === 'on') {
+        $('.resource-card').each(function() {
+            $(this).unbind('click')
+            $(this).attr('state','out')
+            $(this).addClass("border-0")
+            $(this).removeClass("border-success");
+        }) 
+
+        window.localStorage.stagedResources = "[]";
+
+        $(this).html('Make Referral')
+        $(this).attr('state', 'off')
+        $('#commit-referral').css('display', 'none')
+        $('#referral-ins').css('display', 'none')
+    }
+}
+
+function toggleItem(event) {
+    event.preventDefault();
+    state = $(this).attr('state');;
+    id = $(this).attr('id').substring(9);
+
+    stagedResources = localStorage.getItem("stagedResources");
+
+    if (stagedResources.length === 0) {
+        stagedResources = "[]";
+    }
+
+    stagedResources = JSON.parse(stagedResources);
+
+    if (state === 'in') {
+        $(this).attr('state','out');
+        $(this).addClass("border-0");
+        $(this).removeClass("border-success");
+        $(this).removeClass("border-3");
+        stagedResources = stagedResources.filter((inId) => inId != id);
+
+    } else if (state === 'out') {
+        $(this).attr('state','in');
+        $(this).removeClass("border-0");
+        $(this).addClass("border-3")
+        $(this).addClass("border-success");
+
+        stagedResources.push(id);
+
+    }
+
+    let refCount = stagedResources.length
+    document.getElementById("commit-referral").innerHTML = "Select Resources " + "(" + refCount + ")"
+
+    localStorage.stagedResources = JSON.stringify(stagedResources);
+}
+
+function commitReferrals() {
+    stagedResources = window.localStorage.stagedResources
+    if (stagedResources === "[]") return; 
+
+    document.location.href = '/create_referral?resources=' + stagedResources;
+}
