74569cf46c1c647fd9d7e04188d1bcb854de3ef7
Joey Perrino <epiccrashv3@gmail.com>
2020-04-28 22:52:05 -0400

    Commenting finished on .py files
    - Forms have comments
      - Minor username length correction in forms
    - Views commented
    - load_tags_and_resources.py commented

--- NewEra/forms.py
+++ NewEra/forms.py
@@ -7,7 +7,7 @@ import re # Regex matching
 import django_filters
 
 from NewEra.models import User, CaseLoadUser, Resource, Tag, Referral
-# Help from: https://chase-seibert.github.io/blog/2010/05/20/django-manytomanyfield-on-modelform-as-checkbox-widget.html
+# CheckboxSelectMultiple widget from https://chase-seibert.github.io/blog/2010/05/20/django-manytomanyfield-on-modelform-as-checkbox-widget.html
 from django.forms.widgets import CheckboxSelectMultiple
 
 INPUT_ATTRIBUTES = {'class' : 'form-control'}
@@ -15,7 +15,16 @@ MAX_UPLOAD_SIZE = 2500000
 
 # Model Forms
 
+'''
+COMMON NOTES:
+- First names have a 30-digit maximum to match Django's maximum for first names
+- Emails have a 254-digit maximum to reflect the IETF standard (https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address)
+- Phone numbers have an 11-digit maximum to match a 1 and 10 subsequent digits at maximum (methods check for a minimum of 10 digits)
+'''
+
+# Form used to create and edit a CaseLoadUser
 class CaseLoadUserForm(forms.ModelForm):
+	# Set up attributes
 	first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES), required=False)
@@ -23,30 +32,37 @@ class CaseLoadUserForm(forms.ModelForm):
 	nickname = forms.CharField(max_length=100, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES), required=False)
 	notes = forms.CharField(max_length=1000, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES), required=False)
 
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = CaseLoadUser
 		fields = ['first_name', 'last_name', 'nickname', 'email', 'phone', 'notes', 'is_active', 'user']
 		exclude = (
 			'user',
 		)
+		# Textarea widget used to more easily enter notes
 		widgets = {
             'notes': forms.Textarea(attrs={'cols': 120, 'rows': 30}),
         }
 
 	def __init__(self, *args, **kwargs):
 		super(CaseLoadUserForm, self).__init__(*args, **kwargs)
+		# is_active field not defined above so it can be hidden on creation; if the case load user exists, is_active
 		self.fields['is_active'].widget.attrs=INPUT_ATTRIBUTES
 		
+		# Hide the is_active field if the model is being created
 		# https://stackoverflow.com/questions/55994307/exclude-fields-for-django-model-only-on-creation
 		if not self.instance or self.instance.pk is None:
 			for name, field in self.fields.items():
 				if name in ['is_active', ]:
 					field.widget = forms.HiddenInput()
 
+	# Validate the phone number entered
 	def clean_phone(self):
+		# Obtain only the digits entered from the phone
 		phone = self.cleaned_data['phone']
 		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
 
+		# Validate that the phone number is either the standard 10 digits or it is 11 starting with a 1
 		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1')):
 			raise forms.ValidationError('The phone number must be either exactly 10 digits or a 1 followed by 10 digits.')
 
@@ -58,6 +74,7 @@ class CaseLoadUserForm(forms.ModelForm):
 		cleaned_data = super().clean()
 		phone = cleaned_data.get('phone')
 		email = cleaned_data.get('email')
+
 		# Raise an error if neither field is valid
 		if not (phone or email):
 			raise forms.ValidationError('You must input either a phone number or an email address for this user.')
@@ -65,122 +82,78 @@ class CaseLoadUserForm(forms.ModelForm):
 		return cleaned_data
 
 
-# Standard Validation Forms 
-
-class LoginForm(forms.Form):
-	username = forms.CharField(max_length=50, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-	password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
-
-	def clean(self):
-		cleaned_data = super().clean()
-		username = cleaned_data.get('username')
-		password = cleaned_data.get('password')
-
-		user = authenticate(username=username, password=password)
-
-		if not user:
-			raise forms.ValidationError("Invalid Username or Password Entered")
-
-		return cleaned_data # Required by super
-
-
-class RegistrationForm(forms.Form):
-	username   = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-	password  = forms.CharField(max_length=50, label='Password', widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
-	confirm_password  = forms.CharField(max_length=50, label='Confirm Password', widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
-	email      = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
-	first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-	last_name  = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-	phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-
-	class Meta:
-		model = User
-		fields = ['username', 'password', 'confirm_password' 'email', 'first_name', 'last_name', 'phone']
-		exclude = (
-			'is_active',
-		)
-
-	def clean(self):
-		cleaned_data = super().clean()
-		password = cleaned_data.get('password')
-		confirm_password = cleaned_data.get('confirm_password')
-
-		if password and confirm_password and password != confirm_password:
-			raise forms.ValidationError("Passwords did not match.")
-
-		return cleaned_data
-
-	def clean_username(self):
-		username = self.cleaned_data.get('username')
-		if User.objects.filter(username__exact=username):
-			raise forms.ValidationError("Username is already taken.")
-
-		return username
-		
-	def clean_phone(self):
-		phone = self.cleaned_data['phone']
-		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
-
-		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1')):
-			raise forms.ValidationError('The phone number must be either exactly 10 digits or a 1 followed by 10 digits.')
-
-		return cleaned_phone
-
-
+# Form to edit users 
 class EditUserForm(forms.ModelForm):
-	email      = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
+	# Set up attributes
+	email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
 	first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
-	last_name  = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	is_active = forms.BooleanField(required=False)
 
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = User
 		fields = ('email', 'first_name', 'last_name', 'phone', 'is_active')
-
 		exclude = (
+			# A user's username cannot be changed
 			'username',
+			# A user's password and confirmation cannot be changed except through the change password link
 			'password',
 			'confirm_password'
 		)
 
+	# Validate the phone number entered
 	def clean_phone(self):
+		# Obtain only the digits entered from the phone
 		phone = self.cleaned_data['phone']
 		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
 
+		# Validate that the phone number is either the standard 10 digits or it is 11 starting with a 1
 		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1')):
 			raise forms.ValidationError('The phone number must be either exactly 10 digits or a 1 followed by 10 digits.')
 
 		return cleaned_phone
 
 
+# Form to edit the current, logged in user
 class EditSelfUserForm(forms.ModelForm):
-	email      = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
+	# Set up attributes
+	email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
 	first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	last_name  = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = User
 		fields = ('email', 'first_name', 'last_name', 'phone')
 		exclude = (
+			# A user's username cannot be changed
 			'username',
+			# A user's password and confirmation cannot be changed except through the change password link
 			'password',
 			'confirm_password',
+			# A user cannot make themselves inactive
 			'is_active'
 		)
 
+	# Validate the phone number entered
 	def clean_phone(self):
+		# Obtain only the digits entered from the phone
 		phone = self.cleaned_data['phone']
 		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
 
+		# Validate that the phone number is either the standard 10 digits or it is 11 starting with a 1
 		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1')):
 			raise forms.ValidationError('The phone number must be either exactly 10 digits or a 1 followed by 10 digits.')
 
 		return cleaned_phone
 
 
+# Form to create and edit resources
 class CreateResourceForm(forms.ModelForm):
+	# Set up attributes
 	name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 	description = forms.CharField(max_length=1000, required=False, widget=forms.Textarea(attrs=INPUT_ATTRIBUTES))
 	is_active = forms.BooleanField(required=False)
@@ -203,33 +176,38 @@ class CreateResourceForm(forms.ModelForm):
 	# This may or may not be different from the organizational email
 	contact_email = forms.EmailField(max_length=254, required=False, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = Resource
 		fields = ('name', 'description', 'is_active', 'hours', 'email', 'phone', 'extension', 'street', 'street_secondary', 'city', 'state', 'zip_code', 'image', 'url', 'contact_name', 'contact_position', 'fax_number', 'contact_email', 'tags')
+		# content_type is used to manage images, so the user should not manipulate it
 		exclude = (
 			'content_type',
 		)
 
 	def __init__(self, *args, **kwargs):
-        
 		super(CreateResourceForm, self).__init__(*args, **kwargs)
-        
+        # Tags field represented via the CheckboxSelectMultiple() widget
 		self.fields['tags'].widget = CheckboxSelectMultiple()
 		self.fields['tags'].queryset = Tag.objects.all()
 
+		# Hide the is_active field if the model is being created
 		# https://stackoverflow.com/questions/55994307/exclude-fields-for-django-model-only-on-creation
 		if not self.instance or self.instance.pk is None:
 			for name, field in self.fields.items():
 				if name in ['is_active', ]:
 					field.widget = forms.HiddenInput()
 
+	# Validate the resource image
 	def clean_image(self):
 		image = self.cleaned_data['image']
 
 		if image:
+			# Raise an error if the file's extension isn't valid
 			if not image.name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', '.pdf')):
 				raise forms.ValidationError('File type is not image')
 
+			# Raise an error if the file isn't an image or if it is too large
 			try:
 				if (not image.content_type) or (not image.content_type.startswith('image')):
 					raise forms.ValidationError('File type is not image')
@@ -237,48 +215,143 @@ class CreateResourceForm(forms.ModelForm):
 					raise forms.ValidationError('File too big (max: {0} mb)'.format(MAX_UPLOAD_SIZE/1000000))
 			except:
 				pass 
+
 		return image
 
+	# Validate the phone number entered
 	def clean_phone(self):
+		# Obtain only the digits entered from the phone
 		phone = self.cleaned_data['phone']
 		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
 
+		# Validate that the phone number is either the standard 10 digits or it is 11 starting with a 1
 		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1') and not (len(cleaned_phone) == 3 and cleaned_phone[1] == '1' and cleaned_phone[2] == '1')):
 			raise forms.ValidationError('The phone number must be either exactly 10 digits, a 1 followed by 10 digits, or an N-1-1 code of 3 digits.')
 
 		return cleaned_phone
 
+	# Validate the fax number entered
 	def clean_fax_number(self):
+		# Obtain only the digits entered from the fax
 		fax_number = self.cleaned_data['fax_number']
 		cleaned_fax_number = ''.join(digit for digit in fax_number if digit.isdigit())
 
+		# Validate that the fax number is either the standard 10 digits or it is 11 starting with a 1
 		if fax_number and (len(cleaned_fax_number) != 10 and not (len(cleaned_fax_number) == 11 and cleaned_fax_number[0] == '1')):
 			raise forms.ValidationError('The fax number must be either exactly 10 digits or a 1 followed by 10 digits.')
 
 		return cleaned_fax_number
 
 
+# Form only used to edit notes
+class EditReferralNotesForm(forms.ModelForm):
+	# Notes is the only attribute SOWs can freely edit
+	notes = forms.CharField(max_length=1000, required=False, widget=forms.Textarea(attrs=INPUT_ATTRIBUTES))
+
+	# Define the model and fields to include/exclude
+	class Meta:
+		model = Referral
+		fields = ('notes',)
+
+
+# Form to create or edit tags
 class TagForm(forms.ModelForm):
+	# Name is the only attribute
 	name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
 
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = Tag
 		fields = ('name',)
 
 
-# Filter function for the resources
+# Filter function for the resources; needs instantiation as a form
 class ResourceFilter(django_filters.FilterSet):
+	# Tags is the only attribute; uses the CheckboxSelectMultiple widget for easy selection
 	tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget=forms.CheckboxSelectMultiple, label='')
 
+	# Define the model and fields to include/exclude
 	class Meta:
 		model = Resource
 		fields = ('tags',)
 
 
-# Form only used to edit notes
-class EditReferralNotesForm(forms.ModelForm):
-	notes = forms.CharField(max_length=1000, required=False, widget=forms.Textarea(attrs=INPUT_ATTRIBUTES))
+# Standard Validation Forms 
 
+# Form for a user to log in
+class LoginForm(forms.Form):
+	# Set up attributes; username and password are the only ones relevant for login
+	username = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
+
+	# Ensure that the username and password are valid
+	def clean(self):
+		# cleaned_data is necessary to get the fields after they've already been cleaned
+		cleaned_data = super().clean()
+		username = cleaned_data.get('username')
+		password = cleaned_data.get('password')
+
+		# Determine if a user exists with the given username and password
+		user = authenticate(username=username, password=password)
+
+		# Raise an error if the user does not exist
+		if not user:
+			raise forms.ValidationError("Invalid Username or Password Entered")
+
+		return cleaned_data # Required by super
+
+
+# Form for an admin to add a user
+class RegistrationForm(forms.Form):
+	# Set up attributes
+	username = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	password = forms.CharField(max_length=50, label='Password', widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
+	confirm_password = forms.CharField(max_length=50, label='Confirm Password', widget=forms.PasswordInput(attrs=INPUT_ATTRIBUTES))
+	email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs=INPUT_ATTRIBUTES))
+	first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+	phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs=INPUT_ATTRIBUTES))
+
+	# Define the model and fields to include/exclude
 	class Meta:
-		model = Referral
-		fields = ('notes',)
+		model = User
+		fields = ['username', 'password', 'confirm_password' 'email', 'first_name', 'last_name', 'phone']
+		# A user cannot make be made inactive when first created
+		exclude = (
+			'is_active',
+		)
+
+	# Ensure that the user's password is valid
+	def clean(self):
+		# cleaned_data is necessary to get the fields after they've already been cleaned
+		cleaned_data = super().clean()
+		password = cleaned_data.get('password')
+		confirm_password = cleaned_data.get('confirm_password')
+
+		# Raise an error if the password doesn't match the password_confirmation
+		if password and confirm_password and password != confirm_password:
+			raise forms.ValidationError("Passwords did not match.")
+
+		return cleaned_data
+
+	# Ensure that the username is not already used
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+
+		# Raise an error if the username is already in the database
+		if User.objects.filter(username__exact=username):
+			raise forms.ValidationError("Username is already taken.")
+
+		return username
+		
+	# Validate the phone number entered
+	def clean_phone(self):
+		# Obtain only the digits entered from the phone
+		phone = self.cleaned_data['phone']
+		cleaned_phone = ''.join(digit for digit in phone if digit.isdigit())
+
+		# Validate that the phone number is either the standard 10 digits or it is 11 starting with a 1
+		if phone and (len(cleaned_phone) != 10 and not (len(cleaned_phone) == 11 and cleaned_phone[0] == '1')):
+			raise forms.ValidationError('The phone number must be either exactly 10 digits or a 1 followed by 10 digits.')
+
+		return cleaned_phone
--- NewEra/management/commands/load_tags_and_resources.py
+++ NewEra/management/commands/load_tags_and_resources.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from NewEra.models import Resource, Tag
+# Import to read CSV
 import csv
+# Regex matching
 import re
 
 # POPULATE SCRIPT
@@ -9,33 +11,47 @@ class Command(BaseCommand):
     args = '<this func takes no args>'
     help = 'Run this script to create the resources and tags.'
 
+    # Loads the tags and resources 
     def _create_tags_and_resources(self):
 
+        # Open the CSV file
         with open('Northside PD Service Providers.csv', newline='') as csvfile:
+            # Initialize a reader for the CSV
             reader = csv.reader(csvfile, delimiter=',')
+            # Ignore the header row
             header = next(reader, None)
+            # Loop through every row
             for row in reader:
-
+                # Filter through tags and find the first one with a matching name
                 tag = Tag.objects.all().filter(name=row[1]).first()
+
+                # If the tag is not found, create it
                 if (tag == None):
                     tag = Tag.objects.create(name=row[1])
                     tag.save()
 
+                # Replace all non-numeric digits in the phone number
                 phone_formatted = row[5].replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
 
+                # Split the phone nubmer on extension string
                 phone_list = phone_formatted.split('ext.')
+                # Check the size of the new list and set the extension if it exists
                 if len(phone_list) > 1:
                     phone_extension = phone_list[1]
                 else:
                     phone_extension = ''
 
+                # Join the digits of the phone number together to create the string
                 phone_formatted = ''.join(list(filter(str.isdigit, phone_list[0])))
+                # Replace all non-numeric digits in the fax number 
                 fax_formatted = row[6].replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
 
+                # Create a resource from the row columns and give it the tag
                 r = Resource.objects.create(name=row[0], url=row[2], contact_name=row[3], contact_position=row[4], phone=phone_formatted, extension=phone_extension, fax_number=fax_formatted, contact_email=row[7], email=row[7], street=row[8], city=row[9], state=row[10], zip_code=row[11], street_secondary=row[12], description=row[13])
                 r.tags.add(tag)
                 r.save()
 
+        # Print a notice to the developer that resources have been loaded
         print("Resources loaded from CSV.")
 
     def handle(self, *args, **options):
